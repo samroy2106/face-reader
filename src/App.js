faa8c3d239d016fbbcc362b3b4a31a470bc2ad95
Samarth Roy <samarthroy2106@gmail.com>
2021-05-02 20:22:25 +0530

    Added Rank component, started on Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const app = new Clarifai.App({
+  apiKey: 'c4cbfd9ed3b745be8c51e17e0f476478'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 170,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    }
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("", "")
+  }
+
   render () {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
       </div>
     );
   }

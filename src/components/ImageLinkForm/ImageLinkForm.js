d77e65b8929d7872a287c281972d17f4cd869235
Samarth Roy <samarthroy2106@gmail.com>
2021-05-02 15:17:47 +0530

    Add signout and logo components

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p className='f3'>
+        {'This Face Reader will detect faces in your pictures.'}
+      </p>
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;

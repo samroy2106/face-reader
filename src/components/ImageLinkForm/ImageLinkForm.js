fec89a1706f137c197b4833fb9b4990736222a0d
Samarth Roy <samarthroy2106@gmail.com>
2021-05-04 15:08:54 +0530

    Added signin, register forms

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Face Reader will detect faces in your pictures.'}
+        {'This Face Reader will detect a face in a picture.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>

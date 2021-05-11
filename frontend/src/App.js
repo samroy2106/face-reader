3c1ea96e99560bf9b776ff16390da25957f2e9c0
Samarth Roy <samarthroy2106@gmail.com>
2021-05-11 17:27:27 +0530

    Enabled rank feature on image detect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
   }
 
   loadUser = (data) => {
-    this.setState({data: {
+    this.setState({user: {
       id: data.id,
       name: data.name,
       email: data.email,
@@ -83,7 +83,22 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if(response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
